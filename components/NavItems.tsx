bdc19cfc04618d1e83b36e6bdee1708bfb295344
Atomic <atomicisnoah.code@gmail.com>
2025-10-01 09:28:24 +0100

    final fixes for billing and restrictions

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -7,19 +7,19 @@ import {cn} from "@/lib/utils";
 const navItems = [
     { label:'Home', href: '/' },
     { label: 'Companions', href: '/companions' },
-    { label: 'My Journey', href: 'my-journey' },
+    { label: 'Journey', href: 'my-journey' },
 ]
 
 const NavItems = () => {
     const pathname = usePathname();
 
     return (
-        <nav className="flex items-center gap-4">
+        <nav className="flex items-center gap-3">
             {navItems.map(({ label, href }) => (
                 <Link
                     href={href}
                     key={label}
-                    className={cn(pathname === href && 'text-primary font-semibold')}
+                    className={cn(pathname === href && 'text-primary text-xs md:text-sm font-semibold')}
                 >
                     {label}
                 </Link>
